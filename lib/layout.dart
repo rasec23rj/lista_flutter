3985b77789e171d285e0fde21e89ceae1d955430
rasec23rj <rasec23rj@gmail.com>
2020-12-15 10:41:14 -0300

    lista

--- lib/layout.dart
+++ lib/layout.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:lista/pages/about.dart';
 import 'package:lista/pages/config.dart';
@@ -9,12 +10,72 @@ class Layout {
   static final pages = [HomePage.tag, AboutPage.tag, ConfigPage.tag];
 
   static Scaffold getContent(BuildContext context, content) {
+    TextEditingController _nomeController;
+    _nomeController = TextEditingController();
     return Scaffold(
       appBar: AppBar(
         backgroundColor: primary(),
         title: Center(
           child: Text('Lista de mercado'),
         ),
+        actions: <Widget>[
+          GestureDetector(
+            onTap: () {
+              showDialog<void>(
+
+                context: context,
+                barrierDismissible: false, // user must tap button!
+                builder: (BuildContext context) {
+                  return AlertDialog(
+
+                    title: Text('Novo item'),
+                    content: SingleChildScrollView(
+                      child: ListBody(
+                        children: <Widget>[
+
+                          TextField(
+                            controller: _nomeController,
+                            decoration: InputDecoration(
+                              
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30)
+                              ),
+                              labelText: 'Nome',
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    actions: <Widget>[
+                      RaisedButton(
+                        color: secundary(),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: Text(
+                          'Cancelar',
+                        ),
+                      ),
+                      RaisedButton(
+                        color: primary(),
+                        onPressed: () {
+                          print('RaisedButton: ${_nomeController.text}');
+                          Navigator.of(context).pop();
+                        },
+                        child: Text(
+                          'Adicionar',
+                          style: TextStyle(color: Layout.light()),
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
+            child: Icon(Icons.add),
+          ),
+          Padding(padding: EdgeInsets.only(right: 20)),
+        ],
       ),
       bottomNavigationBar: BottomNavigationBar(
           currentIndex: currItem,
